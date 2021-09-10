f4d47da186758c9bcfd0286fbc169a72d53386e2
Matthew Stack <matthewstack@Matthews-MacBook-Pro-4.local>
2021-09-10 11:39:26 -0400

    Set up axios request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
@@ -8,7 +9,20 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, getCharacters] = useState([])
+  useEffect(() => {
+    axios
+      .get(`https://swapi.dev/api/people`)
+      .then((res) => {
+        getCharacters(res.data)
+        console.log(res.data)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }, []);
 
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
